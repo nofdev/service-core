0b94fe80569f08bf2d28563682666bbab5872ac3
mengqiang <mengqiang81@gmail.com>
2016-02-29 16:47:33 +0800

    react sort design

--- src/main/groovy/org/nofdev/servicefacade/Sort.groovy
+++ src/main/groovy/org/nofdev/servicefacade/Sort.groovy
@@ -3,6 +3,7 @@ package org.nofdev.servicefacade
 /**
  * Created by Qiang on 2/29/16.
  */
-class Sort {
-    List<SortOrder> order
+class Sort<T> {
+    List<SortOrder<T>> sortOrders
+    //TODO 需要支持 direction 默认值和直接传入 field 列表
 }
