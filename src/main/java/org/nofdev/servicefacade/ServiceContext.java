10e948d7a04407a6bdcaa05d412769b02e3a8766
tfgzs <867805932@qq.com>
2016-09-23 16:49:48 +0800

    ServiceContext中增加Service-Context-User

--- src/main/java/org/nofdev/servicefacade/ServiceContext.java
+++ src/main/java/org/nofdev/servicefacade/ServiceContext.java
@@ -9,6 +9,7 @@ public class ServiceContext extends HashMap<String, Object> {
 
     public static final String PREFIX = "Service-Context";
     public static final String CALLID = "Service-Context-CallId";
+    public static final String USER = "Service-Context-User";
 
     public void setCallId(CallId callId) {
         put(CALLID, callId);
@@ -17,6 +18,19 @@ public class ServiceContext extends HashMap<String, Object> {
     public CallId getCallId() {
         if (get(CALLID) != null) {
             return (CallId) get(CALLID);
+        } else {
+            return null;
+        }
+    }
+
+
+    public void setUser(User user) {
+        put(USER, user);
+    }
+
+    public User getUser() {
+        if (get(USER) != null) {
+            return (User) get(USER);
         } else {
             return null;
         }
---
+++ src/main/java/org/nofdev/servicefacade/User.java
@@ -0,0 +1,16 @@
+package org.nofdev.servicefacade;
+
+/**
+ * Created by Liutengfei on 2016/9/23 0023.
+ */
+public class User {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
