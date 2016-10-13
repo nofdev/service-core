4278ae51291e7d60841f4901d78aae34b759d6a6
tfgzs <867805932@qq.com>
2016-10-13 14:45:25 +0800

    AbstractBusinessException增加泛型属性datail，用于存储异常的附加信息

--- src/main/groovy/org/nofdev/servicefacade/AbstractBusinessException.groovy
+++ src/main/groovy/org/nofdev/servicefacade/AbstractBusinessException.groovy
@@ -5,8 +5,10 @@ package org.nofdev.servicefacade;
  * <p/>
  * 所有业务系统能够捕获的异常都要转换为具体类型的业务逻辑异常抛出，如OrderNotFoundException
  */
-abstract class AbstractBusinessException extends RuntimeException {
+abstract class AbstractBusinessException<T> extends RuntimeException {
     static String DEFAULT_EXCEPTION_MESSAGE = "业务逻辑异常";
+
+    T datail
 
     AbstractBusinessException() {
         super(DEFAULT_EXCEPTION_MESSAGE)
@@ -23,4 +25,19 @@ abstract class AbstractBusinessException extends RuntimeException {
     AbstractBusinessException(Throwable cause) {
         super(cause)
     }
+    AbstractBusinessException(String message,T datail) {
+        super(message)
+        this.datail=datail
+    }
+
+    AbstractBusinessException(String message, Throwable cause,T datail) {
+        super(message, cause)
+        this.datail=datail
+    }
+
+    AbstractBusinessException(Throwable cause,T datail) {
+        super(cause)
+        this.datail=datail
+    }
+
 }
--- src/main/groovy/org/nofdev/servicefacade/ExceptionMessage.groovy
+++ src/main/groovy/org/nofdev/servicefacade/ExceptionMessage.groovy
@@ -2,7 +2,8 @@ package org.nofdev.servicefacade
 /**
  * Created by Qiang on 7/31/14.
  */
-class ExceptionMessage {
+class ExceptionMessage<T> {
+    T datail
     String name;
     String msg;
     ExceptionMessage cause;
