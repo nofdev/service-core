6c85622a4f90143a727d8b9c01a01c01cb06332f
迟归 <mengq@alibaba-inc.com>
2018-02-01 15:34:00 +0800

    add userdata method

--- src/main/groovy/org/nofdev/servicefacade/ServiceContext.groovy
+++ src/main/groovy/org/nofdev/servicefacade/ServiceContext.groovy
@@ -1,12 +1,42 @@
 package org.nofdev.servicefacade
+
+import groovy.transform.CompileStatic
+
 /**
  * Created by Qiang on 11/4/15.
  */
+@CompileStatic
 public class ServiceContext extends HashMap<String, Object> {
 
     public static final String PREFIX = "Service-Context";
     public static final String CALLID = "Service-Context-CallId";
     public static final String USER = "Service-Context-User";
+
+    public void putUserdata(String key, String value) {
+        this.put("${PREFIX}-${key}".toString(), value)
+    }
+
+    public String getUserdata(String key) {
+        if (this.get("${PREFIX}-${key}")) {
+            this.get("${PREFIX}-${key}")
+        } else {
+            this.get("${PREFIX.toLowerCase()} - ${key}")
+        }
+    }
+
+    public void removeUserData(String key) {
+        this.remove("${PREFIX}-${key}")
+        this.remove("${PREFIX.toLowerCase()}-${key}")
+    }
+
+    public HashMap<String, Object> getUserDataMap() {
+        this.findAll {
+            //UserData不包含 CallId, 因为 CallId 是变化的
+            it.key.toLowerCase().startsWith("${PREFIX.toLowerCase()}") && (it.key.toLowerCase() != "${CALLID.toLowerCase()}")
+        }.collectEntries {
+            [it.key.substring(16), it.value]
+        } as HashMap<String, Object>
+    }
 
     public void setCallId(CallId callId) {
         put(CALLID, callId);
